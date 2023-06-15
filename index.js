13a107770b7198fb978b032937224e84b70e26b1
karanja03 <wambuik03@gmail.com>
2023-06-15 11:55:20 +0300

    complete functions continued

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code your solution here
+function saturdayFun(task){
+    if(task === undefined){
+    return "This Saturday, I want to roller-skate!";
+    }
+    else {
+        return `This Saturday, I want to ${task}!`
+    }
+
+}
+saturdayFun();
+
+ function mondayWork(task){
+    if(task === undefined){
+        return "This Monday, I will go to the office.";
+    }
+    else{
+        return `This Monday, I will ${task}.`;
+    }
+
+ }
+ mondayWork();
+function wrapAdjective(flair = "*"){
+    return function(adjective = "special"){
+        return `You are ${flair}${adjective}${flair}!`;
+
+    }
+}
+wrapAdjective();
